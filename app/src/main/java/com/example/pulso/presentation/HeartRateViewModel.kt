3cb9965b6df45f93448c7daf97a06fe9c8f185c7
Alex Marsden <alex.w.marsden@gmail.com>
2024-04-08 13:09:02 -0400

    Add heart rate view model

---
+++ app/src/main/java/com/example/pulso/presentation/HeartRateViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.pulso.presentation
+
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class HeartRateViewModel {
+    val state: MutableStateFlow<HeartRateState> = MutableStateFlow(HeartRateState.Loading)
+}
+
+sealed class HeartRateState {
+    data object Loading: HeartRateState()
+    data object Content: HeartRateState()
+    data object Error: HeartRateState()
+}
