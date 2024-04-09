089d2089e8f742f3d22e7f089b84c5723be90ac1
Alex Marsden <alex.w.marsden@gmail.com>
2024-04-09 09:31:12 -0400

    Fix style

--- app/src/main/java/com/example/pulso/presentation/HeartRateViewModel.kt
+++ app/src/main/java/com/example/pulso/presentation/HeartRateViewModel.kt
@@ -7,7 +7,7 @@ class HeartRateViewModel {
 }
 
 sealed class HeartRateState {
-    data object Loading: HeartRateState()
-    data object Content: HeartRateState()
-    data object Error: HeartRateState()
+    data object Loading : HeartRateState()
+    data object Content : HeartRateState()
+    data object Error : HeartRateState()
 }
