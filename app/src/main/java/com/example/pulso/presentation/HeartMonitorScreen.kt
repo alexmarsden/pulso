66c8315a768ddbf0944ffbbd72a969ce2196f406
Alex Marsden <alex.w.marsden@gmail.com>
2024-04-20 12:52:10 -0400

    Add hourglass button below heart rate label

--- app/src/main/java/com/example/pulso/presentation/HeartMonitorScreen.kt
+++ app/src/main/java/com/example/pulso/presentation/HeartMonitorScreen.kt
@@ -3,10 +3,17 @@ package com.example.pulso.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.HourglassTop
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.health.services.client.data.DataTypeAvailability
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Icon
 
 @Composable
 fun HeartMonitorScreen(
@@ -22,5 +29,14 @@ fun HeartMonitorScreen(
             availability,
             heartRate
         )
+        Button(
+            onClick = { /* Do something */ },
+        ) {
+            Icon(
+                imageVector = Icons.Default.HourglassTop,
+                contentDescription = "Start timer",
+                modifier = Modifier.size(ButtonDefaults.DefaultIconSize).wrapContentSize(),
+            )
+        }
     }
 }
